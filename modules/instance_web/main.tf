5a6628eec727b57284c2549edcc505ee7a6a57f1
user <user@users-MacBook-Pro.local>
2021-01-19 12:50:05 +0200

    Subnet id should be 1

--- modules/instance_web/main.tf
+++ modules/instance_web/main.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "web" {
   count = var.instance_count
   ami = var.ami
   instance_type = var.instance_type
-  subnet_id = var.subnet_ids
+  subnet_id = var.subnet_ids[0]
   vpc_security_group_ids = [var.security_group]
   user_data = var.filetype == "template" ? templatefile(var.file, local.variables) : file(var.file)
   tags = {
